35d9e8efea16501e2e91541fe3fa886c253d867d
Antonio Carvalho <antoniorcn@hotmail.com>
2021-02-18 17:54:02 -0300

    Setup inicial

---
+++ ads-poo/tarde-aula2/src/edu/fatec/TesteCast.java
@@ -0,0 +1,17 @@
+package edu.fatec;
+
+public class TesteCast {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		
+		float f = (float)50.348;
+		float f2 = 69.93475983f;
+		
+		a = (int)f;
+		double d = 50.792648757329;
+		float f1 = (float)d;
+	}
+
+}
---
+++ ads-poo/tarde-aula2/src/edu/fatec/TesteDeclaracao.java
@@ -0,0 +1,18 @@
+package edu.fatec;
+
+public class TesteDeclaracao {
+	int a = 10; // Variável de Instancia
+	float PI = 3.1415f;
+	int sombra = 111;
+	float salario;
+
+	public void testeCompleto() {
+		int sombra = 222; // Sombreamento de Variáveis
+		sombra = 333;
+	}
+
+	public static void main(String[] args) {
+		int b = 11; // Variável Local
+		double salarioTotal;
+	}
+}
