a263a3b67b4456b10f65a17e25433d62904ebb03
Antonio Carvalho <antoniorcn@hotmail.com>
2021-03-03 22:34:36 -0300

    Noite Aula 4 LES parte 2

--- ads-les/noite-aula4/app1/src/App.js
+++ ads-les/noite-aula4/app1/src/App.js
@@ -3,17 +3,35 @@ import React from 'react';
 const numero = 10;
 
 class TabelaAlunos extends React.Component { 
-
-  lista = [
-    {ra: "0001", nome: "João Silva", nascimento: "05/08/2000"},
-    {ra: "0002", nome: "Maria Silva", nascimento: "09/10/1999"},
-    {ra: "0003", nome: "Pedro Alencar", nascimento: "15/03/2001"},
-  ];
+  state = {
+      lista: [
+        {ra: "0001", nome: "João Silva", nascimento: "05/08/2000"},
+        {ra: "0002", nome: "Maria Silva", nascimento: "09/10/1999"},
+        {ra: "0003", nome: "Pedro Alencar", nascimento: "15/03/2001"},
+        {ra: "0004", nome: "João Alencar", nascimento: "22/03/2001"},
+      ]
+  }
 
   render() { 
+    const listaDisplay = [];
+    for (let i = 0; i < this.state.lista.length; i++) { 
+      listaDisplay.push(
+          <p>{this.state.lista[i].ra} - {this.state.lista[i].nome}</p>
+      );
+    }
     return (
-      <h2>Tabela de Alunos</h2>
-      
+      <div>
+        <h2>Tabela de Alunos</h2>
+        {listaDisplay}
+        <button onClick={
+          ()=>{
+            const novoState = {...this.state};
+            novoState.lista[0].nome="Aluno Novo";
+            this.setState(novoState);
+            console.log(this.state.lista);
+          }
+        }>Alterar</button>
+      </div>
     );
   }
 }
