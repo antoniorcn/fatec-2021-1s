d02e5c5f731937a50e2a797d582900f141952a90
Antonio Carvalho <antoniorcn@hotmail.com>
2021-03-17 18:15:06 -0300

    Aula 5 LES tarde parte 4

---
+++ ads-les/tarde-aula5-parte2/src/edu/curso/Config.java
@@ -0,0 +1,90 @@
+package edu.curso;
+
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@Configuration
+@ComponentScan("edu.curso")
+@EnableWebMvc   
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = {"edu.curso"})
+public class Config {
+	@Bean  
+	public UrlBasedViewResolver setupViewResolver() {  
+		UrlBasedViewResolver resolver = new UrlBasedViewResolver();  
+		resolver.setPrefix("/WEB-INF/jsp/");  
+		resolver.setSuffix(".jsp");  
+		resolver.setViewClass(JstlView.class);  
+		return resolver;  
+	}  
+
+	@Bean
+	public DataSource dataSource() {
+		try {
+			DriverManagerDataSource dataSource = new DriverManagerDataSource();
+			dataSource.setDriverClassName("org.mariadb.jdbc.Driver");
+			dataSource.setUsername("root");
+			dataSource.setPassword("123456");
+			dataSource.setUrl(
+					"jdbc:mariadb://localhost:3306/medicos?createDatabaseIfNotExist=true"); 
+			return dataSource;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	@Bean
+	public Properties hibernateProperties() {
+		Properties hibernateProp = new Properties();
+		hibernateProp.put("hibernate.dialect", 
+			"org.hibernate.dialect.MariaDB53Dialect");
+		hibernateProp.put("hibernate.hbm2ddl.auto", "update");
+		hibernateProp.put("hibernate.format_sql", false);
+		hibernateProp.put("hibernate.use_sql_comments", true);
+		hibernateProp.put("hibernate.show_sql", true);
+		hibernateProp.put("hibernate.max_fetch_depth", 3);
+		hibernateProp.put("hibernate.jdbc.batch_size", 10);
+		hibernateProp.put("hibernate.jdbc.fetch_size", 50);
+		return hibernateProp;
+	}
+	
+	@Bean
+	public EntityManagerFactory entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean factoryBean =
+			new LocalContainerEntityManagerFactoryBean();
+		factoryBean.setPackagesToScan("edu.curso");
+		factoryBean.setDataSource(dataSource());
+		factoryBean.setJpaProperties(hibernateProperties());
+		factoryBean.setJpaVendorAdapter(jpaVendorAdapter());
+		factoryBean.afterPropertiesSet();
+		return factoryBean.getNativeEntityManagerFactory();
+	}
+	
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		return new HibernateJpaVendorAdapter();
+	}
+	
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		return new JpaTransactionManager(entityManagerFactory());
+	}
+}
---
+++ ads-les/tarde-aula5-parte2/src/edu/curso/Medico.java
@@ -0,0 +1,46 @@
+package edu.curso;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy=InheritanceType.JOINED)
+public class Medico {
+	private long id;
+	private String crm;
+	private String nome;
+	private String especialidade;
+	
+	@Id
+	@GeneratedValue
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getCrm() {
+		return crm;
+	}
+	public void setCrm(String crm) {
+		this.crm = crm;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getEspecialidade() {
+		return especialidade;
+	}
+	public void setEspecialidade(String especialidade) {
+		this.especialidade = especialidade;
+	}
+}
