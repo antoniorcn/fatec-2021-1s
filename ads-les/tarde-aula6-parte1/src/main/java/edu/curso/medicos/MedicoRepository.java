9137e0ea21ddb791383198e22f984a08e0a236ef
Antonio Carvalho <antoniorcn@hotmail.com>
2021-03-24 15:58:35 -0300

    LES Tarde Aula 6 parte 1

---
+++ ads-les/tarde-aula6-parte1/src/main/java/edu/curso/medicos/MedicoRepository.java
@@ -0,0 +1,11 @@
+package edu.curso.medicos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MedicoRepository extends JpaRepository<Medico, Long>{
+	
+	public Medico findByCrm(String crm);
+
+}
---
+++ ads-les/tarde-aula6-parte1/src/main/java/edu/curso/medicos/MedicosBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.curso.medicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicosBackendApplication.class, args);
+	}
+
+}
